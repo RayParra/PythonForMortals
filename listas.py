d34998352db36dc11a35fde21ecc15f7f71c8ada
Ray Parra <rparragalaviz@gmail.com>
2018-08-28 12:07:19 -0700

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,25 @@
+#listas.py
+
+# Listas o Array
+
+l = [54, 12, 67, 23]
+
+#print(l)
+
+#l.append(65)
+
+#l.insert(0, 7)
+
+
+
+#print(l[0])
+#print(l[2])
+
+#for item in l:
+#	print(item)
+
+#del(l[1])
+
+print(sorted(l))
+print(l)
+
---
+++ tiposDatos.py
@@ -0,0 +1,26 @@
+
+# Integers
+
+# a = 34
+
+# b = 76
+
+# print(a + b)
+
+# print(a / b)
+# print(a // b)
+
+# Float
+
+# f = 23.89
+# p = 45.32
+
+# print(f * p)
+
+# Boolean
+
+# t = True
+
+# f = False
+
+# print(t , f)
