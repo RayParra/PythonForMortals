786b1e36fc6f12ee49dd99ebca0b5dc357fe2075
Databack <rparragalaviz@gmail.com>
2018-09-06 11:34:45 -0700

    uodate functions and create librerias.py

--- functions.py
+++ functions.py
@@ -2,37 +2,16 @@
 
 
 # # by User
-# def hello():
-# 	return"Hello World! XD"
-# s = hello()
 
-# ss = "Ray Parra"
-# print(s + " - " + ss)
+def hello(name="Anonimo"):
+	return "Hello World!", name
 
-#x = int(input("Escribe un numero"))
-#y = int(input("Escribe otro numero"))
-# def add(num2=1, num=1):
-# 	num = num + num2
-# 	return num
-
-#from mypackage import mylibs
-
-#print(mylibs.restar(x, y))
-
-# print(add(x, y))
+def sumar(x=1, y=2):
+	return x + y
 
 
+x = int(input("Escribe un Numero: ---"))
+y = int(input("Escribe otro Numero: ---"))
 
-# by Python
-# import random, math
-# from math import pi
 
-# for i in range(10):
-# 	print(random.random() * math.cos(i))
-# 	print(pi)
-
-import random
-
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-print(random.choice(lista))
+print(sumar(x, y))
---
+++ librerias.py
@@ -0,0 +1,10 @@
+#librerias.py
+
+import random
+import math
+
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+r = random.choice(lista)
+print(math.sin(r))
+print(r)
